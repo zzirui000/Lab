5fcff4408ecc1c693689a50bc15e0ee860c5f9dd
Zheng Zirui <2209675753@qq.com>
2022-10-27 20:29:26 +0800

    commit4

--- src/lab1_2.java
+++ src/lab1_2.java
@@ -25,23 +25,13 @@ public class lab1_2 {
         String keywords="abstract、assert、boolean、break、byte、case、catch、char、class、continue、default、do、double、else、 enum、extends、final、finally、float、for、if、implements、import、int、interface、instanceof、long、native、new、package、private、protected、public、return、short、static、 strictfp、super、switch、synchronized、this、throw、throws、 transient、try、void、volatile、while";//all keywords
         String []keyArr=keywords.split("、");
 
-        Find_key(keyArr,code);
+        
         Find_SwitchCase(code);
         if_Else_Elseif(code);
     }
 
 
-    public static void Find_key(String[] keyArr,String code){
-        int key_num = 0;
-        for(int i = 0; i<keyArr.length; i++) {
-            Pattern p=Pattern.compile("[^a-z]"+keyArr[i]+"[^a-z]");
-            Matcher matcher=p.matcher(code);
-            while(matcher.find()) {
-                key_num++;
-            }
-        }
-        
-    }
+
 
     public static void Find_SwitchCase(String code){
         //check switch
