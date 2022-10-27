ca47e041c01c6391dd90953fe48be25bd8808ae6
Zheng Zirui <2209675753@qq.com>
2022-10-27 20:30:35 +0800

    commit5

--- src/lab1_2.java
+++ src/lab1_2.java
@@ -22,10 +22,9 @@ public class lab1_2 {
             line=bufferedReader.readLine();
         }
 
-        String keywords="abstract、assert、boolean、break、byte、case、catch、char、class、continue、default、do、double、else、 enum、extends、final、finally、float、for、if、implements、import、int、interface、instanceof、long、native、new、package、private、protected、public、return、short、static、 strictfp、super、switch、synchronized、this、throw、throws、 transient、try、void、volatile、while";//all keywords
-        String []keyArr=keywords.split("、");
-
         
+
+
         Find_SwitchCase(code);
         if_Else_Elseif(code);
     }
