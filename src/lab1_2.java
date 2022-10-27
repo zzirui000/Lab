11e33b23d474081d0f8dc808ef9aacea7cfd6ce1
Zheng Zirui <2209675753@qq.com>
2022-10-27 20:32:05 +0800

    commit7

--- src/lab1_2.java
+++ src/lab1_2.java
@@ -25,8 +25,8 @@ public class lab1_2 {
         String keywords="abstract、assert、boolean、break、byte、case、catch、char、class、continue、default、do、double、else、 enum、extends、final、finally、float、for、if、implements、import、int、interface、instanceof、long、native、new、package、private、protected、public、return、short、static、 strictfp、super、switch、synchronized、this、throw、throws、 transient、try、void、volatile、while";//all keywords
         String []keyArr=keywords.split("、");
 
+        Find_key(keyArr,code);
         
-        Find_SwitchCase(code);
         if_Else_Elseif(code);
     }
 
